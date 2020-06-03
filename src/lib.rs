1a0e07ac3c3a0ec608961c0aff1edf7f6a6f42af
Jitu Das <jitudas@gmail.com>
2020-06-03 01:13:00 -0700

    Split Error out into separate file

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ pub use miniclap_derive::MiniClap;
 use std::error::Error as StdError;
 use std::{cell::RefCell, ffi::OsString, marker::PhantomData, str::FromStr};
 
+pub mod error;
+pub use error::{Error, Result};
+
 pub trait MiniClap: Sized {
     #[inline]
     fn parse_or_exit() -> Self {
@@ -35,101 +38,6 @@ pub trait MiniClap: Sized {
     }
 
     fn __parse_internal(args: &mut dyn Iterator<Item = OsString>) -> Result<Self>;
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
-
-#[derive(Debug)]
-pub enum ErrorKind {
-    ParseFailed,
-    UnknownSwitch,
-    TooManyPositional,
-    MissingRequiredArgument,
-    UnexpectedValue,
-    InvalidUtf8,
-    Other,
-}
-
-#[derive(Debug)]
-pub struct Error {
-    pub message: String,
-    pub kind: ErrorKind,
-    pub source: Option<Box<dyn StdError + 'static>>,
-}
-
-impl Error {
-    pub fn exit(&self) -> ! {
-        eprintln!("error: {}", self.message);
-        std::process::exit(1)
-    }
-
-    pub fn parse_failed(name: &str, err: Box<dyn StdError>) -> Error {
-        Error {
-            message: format!("Invalid value for '{}': {}", name, err),
-            kind: ErrorKind::ParseFailed,
-            source: Some(err),
-        }
-    }
-
-    pub fn unknown_switch(name: &str) -> Error {
-        Error {
-            message: format!("Did not recognize argument '{}'", name),
-            kind: ErrorKind::UnknownSwitch,
-            source: None,
-        }
-    }
-
-    pub fn too_many_positional(arg: &str) -> Error {
-        Error {
-            message: format!("Too many positional arguments, starting with '{}'", arg),
-            kind: ErrorKind::TooManyPositional,
-            source: None,
-        }
-    }
-
-    pub fn missing_required_argument(arg_name: &str) -> Error {
-        Error {
-            message: format!("Missing required argument '{}'", arg_name),
-            kind: ErrorKind::MissingRequiredArgument,
-            source: None,
-        }
-    }
-
-    pub fn unexpected_value(arg_name: &str) -> Error {
-        Error {
-            message: format!("Flag '{}' cannot take a value", arg_name),
-            kind: ErrorKind::UnexpectedValue,
-            source: None,
-        }
-    }
-
-    pub fn invalid_utf8() -> Error {
-        Error {
-            message: "Invalid UTF-8 was detected in one or more arguments".into(),
-            kind: ErrorKind::InvalidUtf8,
-            source: None,
-        }
-    }
-
-    pub fn other<I: Into<String>>(message: I) -> Error {
-        Error {
-            message: message.into(),
-            kind: ErrorKind::Other,
-            source: None,
-        }
-    }
-}
-
-impl StdError for Error {
-    fn source(&self) -> Option<&(dyn StdError + 'static)> {
-        self.source.as_ref().map(|x| x.as_ref())
-    }
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.message)
-    }
 }
 
 pub struct ArgHandlers<'a> {
